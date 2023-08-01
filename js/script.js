ce9364c542ec4ff79782836cc2078ca7fb9e79ea
Rafael Rodrigues Moraes <135690957+RafaRoMoraes@users.noreply.github.com>
2023-07-31 23:37:44 -0300

    222

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ class MobileNavbar{
     }
 
     animateLinks(){
-        this.navLinks.forEach((link) => {
+        this.navLinks.forEach((link, index) => {
             link.style.animation 
             ? (link.style.animation = "")
-            : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
         });
     }
 
