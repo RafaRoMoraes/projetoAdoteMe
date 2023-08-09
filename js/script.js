172c770c15f9ed7cfb56d16487d00b0c9b24cbae
Rafael Rodrigues Moraes <135690957+RafaRoMoraes@users.noreply.github.com>
2023-08-09 19:33:25 -0300

    777

--- js/script.js
+++
@@ -1,4 +0,0 @@
-const mobileMenu = document.querySelector(".menu-mobile");
-const navMenu = document.querySelector(".navbar");
-
-mobileMenu.addEventListener("click", () => navMenu.classList.toggle("active"));
